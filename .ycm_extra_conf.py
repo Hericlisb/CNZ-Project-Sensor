e40440c7f2f9d7bed68730d19108290c84dcb032
Hericlisb <hericlisbravot@gmail.com>
2024-12-14 12:16:39 -0500

    Revert "Add files via upload"
    This reverts commit d496be954f53e0a98e6c8e91d94ae7bce48b5266.

--- .ycm_extra_conf.py
+++
@@ -1,9 +0,0 @@
-
-flags = [
-    '-Iinclude',
-    '-std=gnu++11'
-]
-
-def FlagsForFile(filename, **kwargs):
-    return { 'flags': flags } 
-
--- makefile
+++
@@ -1,8 +0,0 @@
-all:
-	cd bin; make all
-
-clean:
-	cd bin; make clean
-
-install:
-	cd bin; make install
